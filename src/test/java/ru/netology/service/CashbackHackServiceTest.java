02fe742878d3bab77b2ea71df74fb7494d704999
viktoriya maslennikova <libikvikaa@yandex.ru>
2021-01-23 20:46:22 +0500

    Added testNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,24 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldCashbackMoreThanThousand() {
+        assertEquals(service.remain(1001), 999);
+    }
+
+    @Test
+    public void shouldCashbackLessThanThousand() {
+        assertEquals(service.remain(999), 1);
+    }
+
+    @Test
+    public void shouldCashbackEquallyThousand() {
+        assertEquals(service.remain(1000), 0);
+    }
+}
